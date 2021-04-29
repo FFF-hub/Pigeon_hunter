1945e6d2d0d9554b5a388d28d4a5627aaaf06f87
Marek Sarnecki <minesarna@gmail.com>
2021-04-29 21:09:39 +0200

    Added Bot class, and invader movement for the pigeons

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ eenemy_1_x_vel, enemy_2_y_vel = 3, 3
 # classes
 class Entity:
     def __init__(self, name, hp, image, x_pos, y_pos, x_vel, y_vel,
-                x_offset, y_offset, moved):
+                x_offset, y_offset, changed):
         self.name = name
         self.hp = hp
         self.x_pos = x_pos
@@ -47,52 +47,54 @@ class Entity:
         self.image = image
         self.x_offset = x_offset
         self.y_offset = y_offset
-        self.moved = moved
-
+        self.changed = changed
 
     def move(self, left, right, up, down):
         if left:
             self.x_pos -= self.x_vel
-            self.moved = 0
+            self.changed = 0
 
         if right:
             self.x_pos += self.x_vel
-            self.moved = 1
+            self.changed = 1
 
         if up:
             self.y_pos -= self.y_vel
-            self.moved = 2
+            self.changed = 2
 
         if down:
             self.y_pos += self.y_vel
-            self.moved = 3
+            self.changed = 3
 
         if not left and not right and not up and not down:
-            self.moved = 4
+            self.changed = 4
+
+class Bot(Entity):
+    def __init__(self, name, hp, image, x_pos, y_pos, x_vel, y_vel,
+                 x_offset, y_offset, changed, hor_mov, ver_mov):
+        super().__init__(name, hp, image, x_pos, y_pos, x_vel, y_vel,
+                         x_offset, y_offset, changed)
+        self.hor_mov = hor_mov
+        self.ver_mov = ver_mov
+
+    def invader_movement(self, left_lim, right_lim, up_lim, down_lim):
+        if self.x_pos - self.x_vel < left_lim:
+            self.hor_mov = 1
+            self.y_pos += 10
+        if self.x_pos + self.x_vel > right_lim:
+            self.hor_mov = 0
+            self.y_pos += 10
+        if self.y_pos - self.y_vel < up_lim:
+            self.ver_mov = 1
+        if self.y_pos + self.y_vel > down_lim:
+            self.ver_mov = 0
 
 
-"""
-    def border_stop(self, left_lim, right_lim, up_lim, down_lim):
-        if self.x_pos < left_lim:
-            self.x_pos += self.x_vel
-            self.moved = True
-        elif self.x_pos > right_lim:
-            self.x_pos -= self.x_vel
-            self.moved = True
-
-
-        if self.y_pos < up_lim:
-            self.y_pos += self.y_vel
-            self.moved = True
-        elif self.y_pos > down_lim:
-            self.y_pos -= self.y_vel
-            self.moved = True
-"""
 # functions
 def entity_update(entity_img, ent_x, ent_y):
     screen.blit(entity_img, (ent_x, ent_y))
 
-def border_detect(Entity, left_lim, right_lim, up_lim, down_lim):
+def border_stop(Entity, left_lim, right_lim, up_lim, down_lim):
     if Entity.x_pos < left_lim:
         Entity.move(False, True, False, False)
     elif Entity.x_pos > right_lim:
@@ -107,13 +109,22 @@ def border_detect(Entity, left_lim, right_lim, up_lim, down_lim):
 # object creation
 # player
 left_arrow = right_arrow = up_arrow = down_arrow = False
+left_enemy = right_enemy = up_enemy = down_enemy = False
 
 player = Entity('player', 3,
                   pygame.image.load('vis/player_01.png'),
-                  540, 500,
+                  640, 640,
                   5, 5,
                   16, 16,
                   0)
+
+enemy = Bot('enemy', 1,
+               pygame.image.load('vis/pigeon_01.png'),
+               640, 100,
+               3, 3,
+               16, 16,
+               0,
+               0, 0)
 
 # game loop
 while game_running:
@@ -124,6 +135,7 @@ while game_running:
                 game_running = False
         # Keys pressed
         if event.type == pygame.KEYDOWN:
+            # Arrows
             if event.key == pygame.K_LEFT:
                 left_arrow = True
             if event.key == pygame.K_RIGHT:
@@ -132,6 +144,12 @@ while game_running:
                 up_arrow = True
             if event.key == pygame.K_DOWN:
                 down_arrow = True
+
+            # QWER
+            if event.key == pygame.K_q:
+                key_q = True
+
+
         # Keys unpressed
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
@@ -142,17 +160,35 @@ while game_running:
                 up_arrow = False
             if event.key == pygame.K_DOWN:
                 down_arrow = False
+
+            # QWER
+            if event.key == pygame.K_q:
+                key_q = False
         # ------------------------
 
     # player movement
     player.move(left_arrow, right_arrow, up_arrow, down_arrow)
+    # ---------------
 
-    # player border stops
-    border_detect(player, 80, 1200, 80, 640)
+    # enemy AI
+    enemy.invader_movement(80, 1200, 80, 640)
+    if enemy.hor_mov == 1:
+        left_enemy, right_enemy = False, True
+    else:
+        left_enemy, right_enemy = True, False
+    # --------
+
+    # enemy movement
+    enemy.move(left_enemy, right_enemy, up_enemy, down_enemy)
+    # --------------
+
+    # border stops
+    border_stop(player, 80, 1200, 80, 640)
+    border_stop(enemy, 80, 1200, 80, 640)
     # -------------------
 
     # change checks
-    if player.moved < 4:
+    if player.changed < 4 or enemy.changed < 4:
         change = True
     # ---------------
 
@@ -170,13 +206,17 @@ while game_running:
 # update
 
     # draw
-    print(change, player.moved)
     if change:
         entity_update(background, 0, 0)
+
+        entity_update(enemy.image,
+                      enemy.x_pos - enemy.x_offset,
+                      enemy.y_pos - enemy.y_offset)
 
         entity_update(player.image,
                       player.x_pos - player.x_offset,
                       player.y_pos - player.y_offset)
+
         pygame.display.flip()
         change = False
 
