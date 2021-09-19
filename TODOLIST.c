6a58f7109f0e27d229032cda17747acb79bdbdc0
FFF-hub <minesarna@gmail.com>
2021-09-19 20:33:27 +0200

    changed/added some stuff: function for level creation, 			  next level condition and score 			  added TODOLIST

---
+++ TODOLIST.c
@@ -0,0 +1,57 @@
+#TODO LIST
+[xx][ok] - first enemy type, and general enemy class
+
+[xx][ok] - general player class + movement + shooting
+
+[01][xx] - 4 more enemy types, + enemy shooting
+
+[02][xx] - boss
+
+[03][xx] - 3 remaining player abilities
+[03][xx] - powerups
+
+[04][xx] - score + score screen after level completion
+
+[05][xx] - menu
+
+[06][xx] - animations
+
+[07][xx] - sounds
+
+[08][xx] - music
+
+[09][??] - items!
+
+[10][??] - item shop after the boss level
+
+[00][xx] - 
+
+[00][xx] - 
+
+[00][xx] - 
+
+[00][xx] - 
+
+[00][xx] - 
+
+[00][xx] - 
+
+[00][xx] - 
+
+[00][xx] - 
+
+[00][xx] - 
+
+[00][xx] - 
+
+[00][xx] - 
+
+[00][xx] - 
+
+[00][xx] - 
+
+[00][xx] - 
+
+[00][xx] - 
+
+[00][xx] - 
