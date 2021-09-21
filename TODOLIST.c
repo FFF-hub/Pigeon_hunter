3259757d8dd34bb2dc081e224d5a869c6ed56492
FFF-hub <minesarna@gmail.com>
2021-09-21 22:17:24 +0200

    todolist added changes

--- TODOLIST.c
+++ TODOLIST.c
@@ -3,11 +3,32 @@
 
 [xx][ok] - general player class + movement + shooting
 
-[01][xx] - 4 more enemy types, + enemy shooting
+[01][xx] - 3 more enemy types, + enemy shooting
+	2 - ufo pigeon (Pigeon02) - strzela zielonym laserem w 3 kombinacje
+		(w dol, w lewy-dol, w prawy-dol) zajmuje 2 hity do zabicia, porusza
+		sie po okregu	
+		[DONE]
+	
+	3 - chonker pigeon (Pigeon03) - wysrywa jaja ktore wybuchaja i respiom
+		odlamki, porusza sie "sinusoidom" powoli, 4 hity na kil
+		[DONE]
+		
+	4 - black ops pigeon (Pigeon04) - szybki, respi sie randomowo, odbija
+		sie od krawedzi ekranu, strzela seriami po 4 pociski mniej wiecej
+		tam gdzie jest gracz, 3 hity na kil, po dostaniu trafienia od gracza
+		znika plynnie na sekunde, potem pojawia sie plynnie
 
-[02][xx] - boss
-
+[02][xx] - boss (Boss01) - mega chonker 100 trafien, pasek hp, 2 fazy
+		   faza 1: strzelanie w sekwencji seriami po 2, przerwa 1 sekunda
+		   faza 2: sranie jajami wybuchajoncymi po 10/20 , przerwa 2 sekundy 
+			
 [03][xx] - 3 remaining player abilities
+		   uzywanie skilli kosztuje SCORE, za score mozna kupowac itemy
+		   skile maja cooldown
+		   W: laser penetrujacy, burst 0.5s, cd 10s
+		   E: tarcza 1hit, 10s cooldown
+		   R: nuke 100 score, 30s cooldown, dmg 10 w promieniu (respi kolo)
+		   
 [03][xx] - powerups
 
 [04][xx] - score + score screen after level completion
